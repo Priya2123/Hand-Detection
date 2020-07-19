d8d72daac066a1fb0b22fb7cd65120cec627ef81
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-07-19 23:05:37 +0530

    webcam video setup

--- app.js
+++ app.js
@@ -3,3 +3,26 @@ navigator.getUserMedia =
   navigator.webkitGetUserMedia ||
   navigator.mozGetUserMedia ||
   navigator.msGetUserMedia;
+
+const video = document.querySelector("#video");
+const audio = document.querySelector("#audio");
+const canvas = document.querySelector("#canvas");
+const canvas = canvas.getContext("2d");
+
+//detects your hand (trained)
+let model;
+
+handTrack.startVideo(video).then((status) => {
+  if (status) {
+    navigator.getUserMedia(
+      { video: {} },
+      (stream) => {
+        video.srcObject = stream;
+      },
+      (err) => console.log(err)
+    );
+  }
+});
+handTrack.load().then((lmodel) => {
+  model = lmodel;
+});
